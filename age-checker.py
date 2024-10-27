f051a4f9d00227a63cd5486d40fd78eaf56e605c
nazma98 <nazmasarker1311@gmail.com>
2024-10-27 13:43:23 +0600

    add age-checker.py file

---
+++ age-checker.py
@@ -0,0 +1,10 @@
+age = 13
+
+if age >= 18:
+    print("you are an adult")
+elif age > 12:
+    print("you are a teenager")
+elif age > 1:
+     print("you are a child")
+else:
+    print("you are a baby")
