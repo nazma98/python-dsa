ecdf0ab064509da4fb7725d962b45665d460ca7a
nazma98 <nazmasarker1311@gmail.com>
2024-10-28 23:42:38 +0600

    Add complex numbers file
    - Show format of complex numbers
    - Show to separate real and imag. part of the number

---
+++ complex-numbers.py
@@ -0,0 +1,9 @@
+# like complex numbers in mathematics, pyhton supports complex numbers too
+# here the imaginary number is represented by j
+
+num1 = 2 + 3j
+
+print(type(num1)) # prints class complex
+
+num2 = complex(2, 3)
+print(num2.real, num2.imag) # prints 2.0 3.0 as floats
