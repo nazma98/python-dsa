63ef767af826da84a19354126126ff7fc8388332
nazma98 <nazmasarker1311@gmail.com>
2024-10-28 20:46:16 +0600

    Add string using escape character

---
+++ string.py
@@ -0,0 +1,26 @@
+# string can be quoted under single quote or double quote
+# using single quote
+first_name = 'nazma'
+
+# using double quote
+last_name = "sarker"
+
+print(f"My name is {first_name} {last_name}.")
+
+# using escape character to print single/double quote
+# here \ is used as escape character
+name = "Nazma\'s"
+print(name)
+
+mom_quote = 'Mom said,\"Don\'t go outside.\"'
+print(mom_quote)
+
+
+# using \ for a new line inside a string
+new_line = "here\'s a new \nline"
+print(new_line)
+
+
+# using \ for printing a \
+directory = "C\\usrs\\Desktop"
+print(directory)
