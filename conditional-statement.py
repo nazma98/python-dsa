09b3815c257c31556a1679a6611149b6ed9970f3
nazma98 <nazmasarker1311@gmail.com>
2024-10-29 13:15:31 +0600

    Add elif example to check whether a number is positive, negative or zero

--- conditional-statement.py
+++ conditional-statement.py
@@ -4,3 +4,14 @@ if condition:
     print("The condition is true!")
 else:
     print("The condition is false")
+
+
+# elif example
+
+number = 10
+if number > 0:
+    print("it's positive number")
+elif number < 0:
+    print("it's negative number")
+else:
+    print("it's zero")
