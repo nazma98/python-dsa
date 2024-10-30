0af01cf7684d87e4ac08110100f582db099bcb51
nazma98 <nazmasarker1311@gmail.com>
2024-10-30 20:31:17 +0600

    Add functions in python

---
+++ functions.py
@@ -0,0 +1,12 @@
+def say_hi():
+    return "Hi user!"
+
+response = say_hi()
+print(response)
+
+# pass parameter as arg
+
+def greet(name, age):
+    return "Hello "+ name+", you are "+ age
+
+print(greet("Nazma", "25"))
