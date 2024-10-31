8aeb1d22b6038d697891141bb20eb39d08380a25
nazma98 <nazmasarker1311@gmail.com>
2024-10-31 23:59:32 +0600

    Add program to create a new file

--- write-file.py
+++ write-file.py
@@ -2,3 +2,8 @@ employee_file = open("employee.txt", "a")
 employee_file.write("\nKelly - Customer service")
 
 employee_file.close()
+
+# create new file
+fruits_file = open("fruits.txt", "w")
+fruits_file.write("Apple")
+fruits_file.close()
