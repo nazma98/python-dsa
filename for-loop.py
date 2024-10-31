482ef56ffd5c45a3ba4dd30b59e12016da31e389
nazma98 <nazmasarker1311@gmail.com>
2024-10-31 10:29:15 +0600

    Add for loop example using range

--- for-loop.py
+++ for-loop.py
@@ -7,3 +7,16 @@ for letter in "Nazma Sarker":
 friends = ["promi", "mukta", "swarna", "sigma"]
 for friend in friends:
     print(friend)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+# iterate though a range of number
+for index in range(10):
+    print(index)
+
+# iterate though a specified range
+print("New range")
+for num in range(3, 10):
+    print(num)
+
