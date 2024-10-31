2b4a8abf07b22d5cc71c21a0283d98dcc83a2228
nazma98 <nazmasarker1311@gmail.com>
2024-10-31 10:17:14 +0600

    Add for loop example using string and array

---
+++ for-loop.py
@@ -0,0 +1,9 @@
+# iterate through a string
+
+for letter in "Nazma Sarker":
+    print(letter)
+
+# iterate through array
+friends = ["promi", "mukta", "swarna", "sigma"]
+for friend in friends:
+    print(friend)
