814f5683c4e54ce5ba0efa75743bc38c9374a5a5
nazma98 <nazmasarker1311@gmail.com>
2024-10-31 11:49:48 +0600

    Add a translator in python

---
+++ shimul-translator.py
@@ -0,0 +1,35 @@
+# shimul translator is a translator which converts any vowels in a sentence
+# to s
+# for example : Cat -> Cst, neko -> nsks
+
+def translate(phrase):
+    translation = ""
+    vowels=['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    for letter in phrase:
+        if letter in vowels:
+            translation += 's'
+        else:
+            translation += letter
+    return translation
+
+phrase = input("Enter the phrase: ")
+# print(phrase) 
+
+print(translate(phrase))
+
+
+"""
+for letter in phrase:
+    if letter in vowels:
+        print("s")
+
+for index in range(len(phrase)):
+    if phrase[index] in vowels:
+        phrase[index] = "s"
+
+print(phrase)
+"""
+
+
+
