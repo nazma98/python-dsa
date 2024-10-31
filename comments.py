472e8dc48924874de3e21b02a9261f822640d217
nazma98 <nazmasarker1311@gmail.com>
2024-10-31 12:40:36 +0600

    Add example of using comments in python

---
+++ comments.py
@@ -0,0 +1,11 @@
+# this is a single line acomment
+print("This is a comment")
+
+'''
+this is a 
+multi line
+comment
+'''
+
+# standard way to put multi line comment
+# is to use has for multi line
