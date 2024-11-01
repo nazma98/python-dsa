61b6fc6e8cdb6940338062f560b2a79f1db37569
nazma98 <nazmasarker1311@gmail.com>
2024-11-01 21:12:48 +0600

    Add app file to import and create student from Student class

---
+++ app.py
@@ -0,0 +1,8 @@
+# from the student file import Student class
+from student import Student
+
+# Create a student object
+student1 = Student("Nazma", "CSE", 3.64, False)
+
+print(student1.name)
+print(student1.gpa)
