b76c6bce4d4dda79d0ff55d5c1148a31f7a7b3c5
nazma98 <nazmasarker1311@gmail.com>
2024-11-01 21:31:17 +0600

    Add correction to the Question class

--- Question.py
+++ Question.py
@@ -1,3 +1,4 @@
-def __init__(self, prompt, answer):
-    self.prompt = prompt
-    self.answer = answer
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
