9e6df9ed796b97d19c45c218aac28582291fb82c
nazma98 <nazmasarker1311@gmail.com>
2024-11-01 23:54:20 +0600

    Add test method to the program

--- simple-mcq.py
+++ simple-mcq.py
@@ -1,9 +1,28 @@
+from Question import Question
+
 question_prompts = [
-    "What colors are apples?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
-    "What colors are berries?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
-    "What colors are bananas?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
-    "What colors are pineapples?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n"
+    "What colors are apples?\n (a)Red \n (b)Green \n (c)Yellow \n (d)Purple\n",
+    "What colors are berries?\n (a)Red \n (b)Green \n (c)Yellow \n (d)Purple\n",
+    "What colors are bananas?\n (a)Red \n (b)Green \n (c)Yellow \n (d)Purple\n",
+    "What colors are pineapples?\n (a)Red \n (b)Green \n (c)Yellow \n (d)Purple\n"
 
 ]
 
-print(question_prompts)
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "d"),
+    Question(question_prompts[2], "c"),
+    Question(question_prompts[3], "b")
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score +=1 
+
+    print(f"You got {score}/{len(questions)} correct")
+
+
+run_test(questions)
