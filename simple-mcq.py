7f946f1418ec9acb5ccdaf677a01ad8f90960a2a
nazma98 <nazmasarker1311@gmail.com>
2024-11-01 21:25:07 +0600

    Add program for simple mcq question

---
+++ simple-mcq.py
@@ -0,0 +1,9 @@
+question_prompts = [
+    "What colors are apples?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
+    "What colors are berries?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
+    "What colors are bananas?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n",
+    "What colors are pineapples?\n (a)Red \n (b)Green \n (c)Yellow \n (d) Purple\n\n"
+
+]
+
+print(question_prompts)
