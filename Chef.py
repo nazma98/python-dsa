7790610e406a35a6a8cac63d5d9bb3c5b37adc32
nazma98 <nazmasarker1311@gmail.com>
2024-11-02 18:58:39 +0600

    Add new class Chef

---
+++ Chef.py
@@ -0,0 +1,9 @@
+class Chef:
+    def make_rice(self):
+        print("Chef makes rice!")
+
+    def make_chicken(self):
+        print("Chef makes checken!")
+
+    def make_dumpling(self):
+        print("Chef makes dumplings!")
