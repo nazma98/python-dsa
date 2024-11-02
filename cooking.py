21be4ab73e80b3a6846070a134a24e5485b14fff
nazma98 <nazmasarker1311@gmail.com>
2024-11-02 19:36:21 +0600

    Add objects of subclass to demonstrate inheritance

--- cooking.py
+++ cooking.py
@@ -1,5 +1,13 @@
 from Chef import Chef
+from ChineseChef import ChineseChef
 
-newChef = Chef()
+new_Chef = Chef()
 
-newChef.make_chicken()
+new_Chef.make_chicken()
+new_Chef.make_special_dish()
+
+my_chinese_Chef = ChineseChef()
+
+my_chinese_Chef.make_fried_rice()
+my_chinese_Chef.make_chicken()
+my_chinese_Chef.make_special_dish()
