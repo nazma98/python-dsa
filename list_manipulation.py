b116465063f8ada74fdfd16ee78d5188fd0f3586
nazma98 <nazmasarker1311@gmail.com>
2024-11-04 19:54:22 +0600

    Add program to find sum and avg of list of numbers

---
+++ list_manipulation.py
@@ -0,0 +1,10 @@
+# Given a list of numbers, find the sum and average of all numbers.
+list = [12, 43, 54, 121, 32, 65, 78, 87]
+sum =0
+for i in list:
+    sum = sum + i
+
+print(f"Sum {sum}")
+print(f"Average {sum / len(list)}")
+# Write a program to find the maximum and minimum in a list without using max() or min().
+# Remove duplicates from a list of integers.
