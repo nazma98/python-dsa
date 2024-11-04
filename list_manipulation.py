f8f3473ea16ab6a8d29d7d5d0ed9a251ff31565a
nazma98 <nazmasarker1311@gmail.com>
2024-11-04 20:01:58 +0600

    Add program to find max and min in a list

--- list_manipulation.py
+++ list_manipulation.py
@@ -7,4 +7,15 @@ for i in list:
 print(f"Sum {sum}")
 print(f"Average {sum / len(list)}")
 # Write a program to find the maximum and minimum in a list without using max() or min().
+max = list[0]
+min = list[0]
+for i in range(1, len(list)):
+    if list[i] > max:
+        max = list[i]
+
+    if list[i]<min:
+        min = list[i]
+
+print(f"Maximum number is {max}")
+print(f"Minimum number is {min}")
 # Remove duplicates from a list of integers.
