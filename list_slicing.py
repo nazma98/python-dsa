3dc79eeeaeaa107924f8afe35105b7b4341dfac3
nazma98 <nazmasarker1311@gmail.com>
2024-11-05 20:22:12 +0600

    Extract sublist of first 5 elements

--- list_slicing.py
+++ list_slicing.py
@@ -5,6 +5,10 @@ natural_num = list(range(1,21))
 print(natural_num)
 
 #     Extract a sublist of the first 5 elements.
+print(natural_num[:5])
+
+
 #     Extract the last 5 elements of the list.
+
 #     Extract every other element from the list.
 #     Reverse the list using slicing.
