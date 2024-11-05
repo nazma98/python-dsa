5b4cfb79de4a1527314182592add2ef5ec71cfe2
nazma98 <nazmasarker1311@gmail.com>
2024-11-05 20:18:04 +0600

    Add python program to create list of first 20 natural numbers

---
+++ list_slicing.py
@@ -0,0 +1,10 @@
+# List Slicing
+
+#     Create a list of the first 20 natural numbers.
+natural_num = list(range(1,21))
+print(natural_num)
+
+#     Extract a sublist of the first 5 elements.
+#     Extract the last 5 elements of the list.
+#     Extract every other element from the list.
+#     Reverse the list using slicing.
