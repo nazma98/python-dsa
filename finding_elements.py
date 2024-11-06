0b4f3570aa76267888c7819feb4c9941d4ff34b2
nazma98 <nazmasarker1311@gmail.com>
2024-11-06 20:59:17 +0600

    Add file to find elements in list

---
+++ finding_elements.py
@@ -0,0 +1,4 @@
+# Write a program to find the index of a given element in a list.
+list = [1, 3,5, 8, 54, 23, 89]
+# Count the occurrences of a specific element in a list.
+# Find the second largest element in a list of numbers.
