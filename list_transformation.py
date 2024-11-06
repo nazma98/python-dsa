67367523c7094520972fe301d42220f80e8ef856
nazma98 <nazmasarker1311@gmail.com>
2024-11-06 21:00:28 +0600

    Add file list transformation

---
+++ list_transformation.py
@@ -0,0 +1,2 @@
+# Given a list of names, capitalize the first letter of each name.
+# Given a list of temperatures in Celsius, convert each temperature to Fahrenheit and store the results in a new list.
