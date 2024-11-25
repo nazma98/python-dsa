26bd6d5c797cd1db83fc144e09d23ef8f3fa687e
nazma98 <nazmasarker1311@gmail.com>
2024-11-25 23:09:27 +0600

    Print value

--- dictionary.py
+++ dictionary.py
@@ -4,3 +4,6 @@ print(new_dictionary)
 # use dict() function to create dictionary
 new_dictionary2 = dict(name = "Kopi", age=2, owner="Shishir")
 print(new_dictionary2)
+
+name = new_dictionary["name"]
+print(name)
