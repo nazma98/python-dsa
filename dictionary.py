fba81a3cda472b7ce812b643c487b72e231ed97d
nazma98 <nazmasarker1311@gmail.com>
2024-11-25 23:07:33 +0600

    Add dict function to create new dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,6 @@
 new_dictionary = {"name": "Bondo", "age":3, "owner": "Shimul-san"}
 print(new_dictionary)
 
+# use dict() function to create dictionary
+new_dictionary2 = dict(name = "Kopi", age=2, owner="Shishir")
+print(new_dictionary2)
