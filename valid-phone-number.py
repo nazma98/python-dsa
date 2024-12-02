6a2a62196fc73b369fd7525826ae14d12624358b
nazma98 <nazmasarker1311@gmail.com>
2024-12-02 22:59:47 +0600

    Add phone number validity check function

--- valid-phone-number.py
+++ valid-phone-number.py
@@ -2,3 +2,7 @@ import re
 
 def is_valid_phone_number(phone_number):
     pattern = r'^(\+8801\d{9}|8801\d{9}|01\d{9})$'
+
+    if re.fullmatch(pattern, phone_number):
+        return True
+    return False
