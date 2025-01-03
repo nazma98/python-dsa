f3bf59f076302d9a5408ad0e0d85f4d3cc134d03
nazma98 <nazmasarker1311@gmail.com>
2025-01-03 23:37:53 +0600

    Sort list

--- lambda-fun-prac.py
+++ lambda-fun-prac.py
@@ -4,3 +4,9 @@ print(add10(5))
 
 mult = lambda x,y: x*y
 print(mult(2, 7))
+
+point2d = [(1,2), (15, 1), (5, -1), (10, 4)]
+point2d_sorted = sorted(point2d)
+
+print(point2d)
+print(point2d_sorted)
